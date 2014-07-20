dd5009fa90f8c36bd684ae82274f61ef43d4d777
Valentin Vago <zeropaper@irata.ch>
2014-07-20 12:09:50 +0200

    lazy commit

---
+++ client/scripts/index.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+  'use strict';
+
+  var FFWD = config.FFWD;
+
+  // var CodeMirror = require('codemirror');
+
+  // FFWD.make('view', 'codeEditor', {
+  //   tagName: 'textarea',
+
+  //   initialize: function() {
+  //     this.editor = CodeMirror.fromTextArea(this.el, {
+  //       mode: 'markdown',
+  //       lineNumbers: true,
+  //       gutters: ['CodeMirror-linenumbers', 'breakpoints']
+  //     });
+  //   }
+  // }, {});
+};
